5edfc6c9f4f0b90feb7a5e71f207ffa93cec9194
thbasse <thbasse@student.42.fr>
2024-09-13 16:49:41 +0200

    update

--- mandatory_srcs/client_srcs/main.c
+++ mandatory_srcs/client_srcs/main.c
@@ -6,46 +6,51 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:07:09 by thbasse           #+#    #+#             */
-/*   Updated: 2024/09/12 19:44:08 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/09/13 16:21:24 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minitalk.h"
 
+
 void	acknowledgement_handler(int signum)
 {
+	static int	received = 0;
+	
 	if (signum == SIGUSR1)
+		++received;
+	else
 	{
-		kill(getpid(), SIGUSR2);
-	}
-}
-
-void	send_char(int server_pid, char c)
-{
-	int bit;
-
-	bit = 7;
-	while (bit >= 0)
-	{
-		if ((c >> bit) & 1)
-			kill(server_pid, SIGUSR2);
-		else
-			kill(server_pid, SIGUSR1);
-		pause();
-		bit--;
+		exit(0);
 	}
 }
 
 void	send_message(int server_pid, const char *message)
 {
-	int i = 0;
-
+	int i;
+	int bit;
+	
+	i = 0;
 	while (message[i] != '\0')
 	{
-		send_char(server_pid, message[i]);
+		bit = 7;
+		while (bit >= 0)
+		{
+			if (message[i] >> bit & 1)
+				kill(server_pid, SIGUSR2);
+			else
+				kill(server_pid, SIGUSR1);
+			usleep(100);
+			bit--;
+		}
 		i++;
 	}
-	send_char(server_pid, '\0');
+	bit = 7;
+	while (bit >= 0)
+	{
+		kill(server_pid, SIGUSR1);
+		usleep(100);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -55,11 +60,13 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 	{
 		printf("Usage: %s <server_pid> <message>\n", argv[0]);
-		return 1;
+		return (1);
 	}
 	server_pid = ft_atoi(argv[1]);
 	signal(SIGUSR1, acknowledgement_handler);
 	signal(SIGUSR2, acknowledgement_handler);
 	send_message(server_pid, argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
